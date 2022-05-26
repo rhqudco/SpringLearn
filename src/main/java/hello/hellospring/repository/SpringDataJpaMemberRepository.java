3c54cc502f52b82fc34c167c8f4004fffb41521d
rhqudco <rhqudco1204@naver.com>
2022-05-26 23:16:18 +0900

    minimal

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface SpringDataJapMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
 
     @Override
     Optional<Member> findByName(String name);
