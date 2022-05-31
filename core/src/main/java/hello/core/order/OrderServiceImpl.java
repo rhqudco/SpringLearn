06e0493bd7348ccbabaef525465bf13380104cd2
rhqudco <rhqudco1204@naver.com>
2022-05-31 17:27:22 +0900

    Only Java Code OOP2

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        // OrderService에서는 Discount에 대한 정보가 없고
+        // DiscountPolicy가 알아서 계산해 줘서 나한테 줘라! 라는 설계로 구현 했기 때문에 단일 책임의 원칙을 잘 지킴
+        // 단일 책임의 원칙을 지키지 못 하면 할인 정책에 대한 변경이 생겼을 때 OrderService를 수정해야 하는 불상사 발생
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
---
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberSerivce;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberSerivce memberSerivce = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberSerivce.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
