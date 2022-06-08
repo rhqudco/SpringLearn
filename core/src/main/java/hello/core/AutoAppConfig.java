bd03f289b64fcf223f3716351d25cb6882994821
rhqudco <rhqudco1204@naver.com>
2022-06-09 00:48:00 +0900

    ComponentScan End

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -13,5 +16,8 @@ import static org.springframework.context.annotation.ComponentScan.*;
 )
 // (excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))는 원래 AppConfig를 제외하기 위함
 public class AutoAppConfig {
-
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
