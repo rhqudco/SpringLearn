546835f82458b3bdae60c4aa870d4399ed2b9eff
rhqudco <rhqudco1204@naver.com>
2022-06-14 01:30:37 +0900

    Spring JPA 1 Setting End

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository { // DAO와 비슷, Entity를 찾아줌
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
