5b807a9cb9403207d4a6ba4aa7059717cc9807f4
rhqudco <rhqudco1204@naver.com>
2022-06-21 01:10:07 +0900

    JPA basic Hello JPA Dev

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // 앤티티 팩토리 매니저
+        EntityManager em = emf.createEntityManager(); // 앤티티 매니저
+
+        EntityTransaction tx = em.getTransaction(); // 트랜잭션
+        tx.begin();
+        try {
+            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
