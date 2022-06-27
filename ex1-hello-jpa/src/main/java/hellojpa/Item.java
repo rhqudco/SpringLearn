15a13c9ada7cb9d4c2b02143911261dae7ee52dd
rhqudco <rhqudco1204@naver.com>
2022-06-28 00:21:26 +0900

    Joined

--- ex1-hello-jpa/src/main/java/hellojpa/Item.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Item.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(name= "DTYPE")
 @Getter @Setter
 public class Item {
