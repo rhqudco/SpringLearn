9501809daf4af89c7851f02772d63729d039aaaf
rhqudco <rhqudco1204@naver.com>
2022-07-05 23:32:19 +0900

    fetch join end

--- jpql/src/main/java/jpql/Team.java
+++ jpql/src/main/java/jpql/Team.java
@@ -2,6 +2,7 @@ package jpql;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,5 +19,6 @@ public class Team {
     private String name;
 
     @OneToMany(mappedBy = "team")
+//    @BatchSize(size = 100)
     private List<Member> members = new ArrayList<>();
 }
